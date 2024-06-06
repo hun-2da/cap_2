068018aa34e24e3bec32f713cf0ea172fe7c564e
20gns <b_h14@naver.ccom>
2024-06-07 02:57:59 +0900

    라스트트2

--- app/src/main/java/codingadventure/community/myapp/FirebasePack/QueryPack/CommunityQuery.java
+++ app/src/main/java/codingadventure/community/myapp/FirebasePack/QueryPack/CommunityQuery.java
@@ -22,7 +22,7 @@ public class CommunityQuery {
     public static Query getMyCommunityQuery(long Temperature){
         Query query = FirebaseUtils.getFirestore()
                 .collection(FirebaseDBNameClass.COMMUNITY_COLLECTION)
-                .whereLessThan(FirebaseDBNameClass.COMMUNITY_limit, Temperature)
+                .whereLessThanOrEqualTo(FirebaseDBNameClass.COMMUNITY_limit, Temperature)
                 .orderBy(FirebaseDBNameClass.COMMUNITY_COMMENT_COUNT, Query.Direction.ASCENDING)  // 댓글 수에 대해 오름차순 정렬
                 .orderBy(FirebaseDBNameClass.COMMUNITY_DATE, Query.Direction.DESCENDING)
                 .limit(7);  // 한번에 몇개의 다이어리를 보여줄 것 인가에 대한 코드
